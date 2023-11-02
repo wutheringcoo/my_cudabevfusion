5e3870a81b1993a43204daf3ac4a427bda223922
wutheringcoo <wutheringcoo@163.com>
2023-11-02 21:21:53 +0800

    add googletest third party

--- bevfusion/modules/image_preprocess/unit_tests/nn_interpol_base_test.cpp
+++ bevfusion/modules/image_preprocess/unit_tests/nn_interpol_base_test.cpp
@@ -1,11 +1,7 @@
-#include < gtest / gtest.h>
+#include <gtest/gtest.h>
 #include <iostream>
-#include "bevfusion/modules/image_preprocess/nn_interpol_base.h"
+// #include "bevfusion/modules/image_preprocess/nn_interpol_base.h"
 using namespace std;
-int main() {
-    cout << "xxxxxx" << endl;
-    return 0;
-}
 
 TEST(test1, nn_inter_test) {
     EXPECT_EQ(2, 2);
